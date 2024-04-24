0dc580cda00ce2f337de2f1ab713e34561e56366
S2HP <112924615+S2HP@users.noreply.github.com>
2024-04-24 15:49:05 +0300

    a lot.

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index_page():
-    return render_template("mainpage.html")
-def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/basics')
-def basics():
-    return render_template('basics.html')
+@app.route('/information')
+def information():
+    return render_template('information.html')
 
-@app.route('/detailedInfo')
-def detailedInfo():
-    return render_template('detailedInfo.html')
+@app.route('/videodetail')
+def videodetail():
+    return render_template('video-detail.html')
+
+@app.route('/photodetail')
+def photodetail():
+    return render_template('photo-detail.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
 
 app.run(host='0.0.0.0', port=81)
